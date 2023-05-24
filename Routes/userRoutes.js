142ef11381c52998d6151f819fc2b1e6fbee4b5a
Hamza Tanveer <hamza.tanveer@emerald-labs.com>
2023-05-24 15:33:17 +0500

    added users crud apis

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const userController = require("../Controllers/userController");
-const { signup, login, verifyTwoFAToken, getPasswordResetLink, resetPassword } = userController;
+const {
+  signup,
+  login,
+  verifyTwoFAToken,
+  getPasswordResetLink,
+  resetPassword,
+  updateUser,
+  getUserById,
+  getAllUsers,
+  deleteUserById,
+} = userController;
 const userAuth = require("../Middlewares/userAuth");
 const router = express.Router();
 
@@ -9,5 +19,9 @@ router.post("/login", login);
 router.post("/verify-2fa", verifyTwoFAToken);
 router.post("/get-password-reset-link", getPasswordResetLink);
 router.post("/reset-password", resetPassword);
+router.put("/:id", updateUser);
+router.get("/:id", getUserById);
+router.get("/", getAllUsers);
+router.delete("/:id", deleteUserById);
 
 module.exports = router;
